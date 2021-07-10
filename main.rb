330e38ca200c5409e9d8f263c51ba821fb130f8e
Regi Fronda <regisfronda@gmail.com>
2021-07-10 02:24:05 -0700

    Add each_with_index caling for comparison to my_each_with_index

--- main.rb
+++ main.rb
@@ -6,9 +6,14 @@ module Enumerable
       count+=1
     end
   end
+
+
 end
 
 puts "my_each vs. each"
 numbers = [1, 2, 3, 4, 5]
 numbers.my_each { |item| puts item }
 numbers.each { |item| puts item }
+
+puts "my_each_with_index vs. each_with_index"
+numbers.each_with_index { |item, index| puts "#{index}: #{item}" }
