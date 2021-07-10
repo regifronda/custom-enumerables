39e2d41e518b4d87e339889b512c85d429edb280
Regi Fronda <regisfronda@gmail.com>
2021-07-10 03:57:25 -0700

    Add my_select method

--- main.rb
+++ main.rb
@@ -15,6 +15,13 @@ module Enumerable
     end
   end
 
+  def my_select
+    array = Array.new
+    self.my_each do |item|
+      array << item if yield(item)
+    end
+    array
+  end
 end
 
 puts "my_each vs. each"
@@ -22,6 +29,10 @@ numbers = [1, 2, 3, 4, 5]
 numbers.my_each { |item| puts item }
 numbers.each { |item| puts item }
 
-puts "my_each_with_index vs. each_with_index"
-numbers.my_each_with_index { |item, index| puts "#{index}: #{item}" }
-numbers.each_with_index { |item, index| puts "#{index}: #{item}" }
+# puts "my_each_with_index vs. each_with_index"
+# numbers.my_each_with_index { |item, index| puts "#{index}: #{item}" }
+# numbers.each_with_index { |item, index| puts "#{index}: #{item}" }
+
+puts "my_select vs. select"
+numbers.my_select { |item| puts item.even? }
+numbers.select { |item| puts item.even? }
